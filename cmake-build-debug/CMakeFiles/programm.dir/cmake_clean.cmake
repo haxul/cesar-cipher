6f2c941b6f93748c540e3ab789d41be007782b1f
haxul <s.s.starodubov@yandex.ru>
2019-11-28 21:19:11 +0400

    init

---
+++ cmake-build-debug/CMakeFiles/programm.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/programm.dir/main.cpp.o"
+  "programm"
+  "programm.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/programm.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
