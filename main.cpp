fdf7c00f6501089a4d5e19cd4a41214caec950aa
haxul <s.s.starodubov@yandex.ru>
2019-11-28 22:58:37 +0400

    lesson 1

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
 
+using namespace std;
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    unsigned short hel = 5;
+    char a = 'a';
+    int c = INT32_MAX;
+    int b {};
+    auto h = 0;
+    cout << (int) 8.0 / 3 << endl << sizeof(h);
+
     return 0;
 }
