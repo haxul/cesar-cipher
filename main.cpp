f744a0a15864d18ea6a2af6bd3f6d71332b4a807
haxul <s.s.starodubov@yandex.ru>
2019-12-03 23:05:57 +0400

    templates

--- main.cpp
+++ main.cpp
@@ -30,8 +30,38 @@ private:
     friend B;
 };
 
+template <typename T>
+T bar(T a, T b) {
+    return a + b;
+}
+template <typename T1, typename T2>
+auto bar(T1 a , T2 b) -> decltype(a+b) {
+    return a + b;
+}
+template <typename K = int>
+class Test {
+public:
+    void process() {
+        cout << a * 100 << endl;
+    }
+    K a;
+};
+
+template <>
+class Test<string> {
+public:
+    void process() {
+        cout << a + a << endl;
+    }
+    string a;
+};
 int main() {
-    Number number1 = Number(3);
+    Test<string> test;
+    test.a = "hello";
+    test.process();
+    cout << bar(1, 2) << endl;
+    cout << bar(1, 1.1) << endl;
+    Number number1(3);
     cout << number1 << endl;
     Number* number = new Number(10);
     cout << *number << endl;
