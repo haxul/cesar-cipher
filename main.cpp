d8969554b8e9e33d6226468f3f5ac29aa3dda568
haxul <s.s.starodubov@yandex.ru>
2019-12-03 21:33:46 +0400

    interfaces

--- main.cpp
+++ main.cpp
@@ -7,7 +7,22 @@ void foo  (Mammal* mammal) {
     mammal->say();
 }
 int Mammal::data = 500;
+
+class A {
+public:
+    virtual void foo() = 0;
+};
+
+class B : public A {
+public:
+    void foo() {
+        cout << "hello foo" << endl;
+    }
+};
+
 int main() {
+    B b;
+    b.foo();
     cout << Mammal::data << endl;
     cout << Mammal::sumo << endl;
     Mammal* mammal = new Cat();
